bec8dd46ce06df3ac44af5c2081e367dfae85ce5
OlehOpara <101872444+OlehOpara@users.noreply.github.com>
2022-11-14 11:29:26 +0100

    Add files via upload

--- mailer/smart.php
+++ mailer/smart.php
@@ -14,7 +14,7 @@ $mail->isSMTP();                                      // Set mailer to use SMTP
 $mail->Host = 'smtp.gmail.com';  // Specify main and backup SMTP servers
 $mail->SMTPAuth = true;                               // Enable SMTP authentication
 $mail->Username = 'olegopara20@gmail.com';                 // Наш логин
-$mail->Password = 'Oleg0529996874';                           // Наш пароль от ящика
+$mail->Password = 'fgna okmn iged gfjy';                           // Наш пароль от ящика
 $mail->SMTPSecure = 'ssl';                            // Enable TLS encryption, `ssl` also accepted
 $mail->Port = 465;                                    // TCP port to connect to
  
